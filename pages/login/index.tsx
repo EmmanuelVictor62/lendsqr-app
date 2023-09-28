3dd46550b5d74d6b749fbbdf16242d8cecc40687
royalManuel <royalmanuel62@gmail.com>
2023-09-28 22:16:42 +0100

    Updated font and login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -24,34 +24,36 @@ const LoginPage: React.FC<loginPageProps> = () => {
       </div>
 
       <div className={styles["login-page__form-wrapper"]}>
-        <div className={styles["login-page__form-heading-container"]}>
-          <h1 className={styles["login-page__form-heading"]}>Welcome</h1>
-          <p className={styles["login-page__form-subheading"]}>
-            Enter details to login
-          </p>
-        </div>
         <div className={styles["login-page__form-container"]}>
-          <div className={styles["login-page__form-input-container"]}>
-            <input
-              type="text"
-              className={styles["login-page__form-input"]}
-              placeholder="Email"
-            />
-            <input
-              type="text"
-              placeholder="Password"
-              className={styles["login-page__form-input"]}
-            />
+          <div className={styles["login-page__form-heading-container"]}>
+            <h1 className={styles["login-page__form-heading"]}>Welcome!</h1>
+            <p className={styles["login-page__form-subheading"]}>
+              Enter details to login
+            </p>
           </div>
+          <div className={styles["login-page__form-container"]}>
+            <div className={styles["login-page__form-input-container"]}>
+              <input
+                type="text"
+                className={styles["login-page__form-input"]}
+                placeholder="Email"
+              />
+              <input
+                type="text"
+                placeholder="Password"
+                className={styles["login-page__form-input"]}
+              />
+            </div>
 
-          <div className={styles["login-page__form-button"]}>
-            <Button
-              type="button"
-              variant="primary"
-              label="LOG IN"
-              name="button"
-              handleClickEvent={() => console.log("Button Clicked")}
-            />
+            <div className={styles["login-page__form-button"]}>
+              <Button
+                type="button"
+                variant="primary"
+                label="LOG IN"
+                name="button"
+                handleClickEvent={() => console.log("Button Clicked")}
+              />
+            </div>
           </div>
         </div>
       </div>
